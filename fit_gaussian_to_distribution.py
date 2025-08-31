f41061a80517c782dc84a00cd1b2ed8c0732503e
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-08-31 17:53:03 +0300

    fix synax error

--- fit_gaussian_to_distribution.py
+++ fit_gaussian_to_distribution.py
@@ -36,7 +36,7 @@ with st.form("myform"):
             raw_data = [float(i) for i in input_stream[1::2]]
             hist, bin_edges = np.histogram(raw_data, bins='auto', density=True)
             bin_mid_points = [0.5*(bin_edges[i] + bin_edges[i+1]) for i in range(0, len(hist))]
-            st.session_state['data'] = pd.DataFrame({'bin_edges': bin_edges, 'bin_mid_points' = bin_mid_points, 'hist': hist})
+            st.session_state['data'] = pd.DataFrame({'bin_edges': bin_edges, 'bin_mid_points': bin_mid_points, 'hist': hist})
         else:
             st.write("you need to upload a valid txt or csv file")
 
