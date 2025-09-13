4c9318d32e1d150d31f4ace3736297374b6b6557
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-09-13 16:22:44 +0200

    Update 1_homepage.py

--- 1_homepage.py
+++ 1_homepage.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.set_page_config(page_title='DPD multipage application')
 
 st.title('Homepage')
+
+st.text('It provides a few tools for the derivation of DPD models')
+
+st.text("This WebApp was created by Evangelos Voyiatzis.")
