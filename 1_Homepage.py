2285c4753cc6be5da613a0c076ada0785a0d6f08
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-09-18 17:10:52 +0200

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -6,4 +6,9 @@ st.title('Homepage')
 
 st.text('It provides a few tools for the derivation of DPD models')
 
+st.text("The standard DPD parametrization App derives the parameters of DPD models using the Groot De Warren methodology. "
+        "The reference is 'Dissipative particle dynamics: "
+        "Bridging the gap between atomistic and mesoscopic simulation' "
+        "J. Chem. Phys. DOI:10.1063/1.474784")
+
 st.text("This WebApp was created by Evangelos Voyiatzis.")
