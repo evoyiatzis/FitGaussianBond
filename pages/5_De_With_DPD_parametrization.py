095dae33cca491b0ab33a1770c222d1bed1fbe5a
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-09-20 19:02:06 +0200

    Update 5_De_With_DPD_parametrization.py

--- pages/5_De_With_DPD_parametrization.py
+++ pages/5_De_With_DPD_parametrization.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.header("Parametrization of DPD models using the De With approach")
+
+st.text("This is a WebApp for the parametrization of DPD models using the de With methodology. "
+        "The reference is 'A generalized method for parameterization of dissipative particle dynamics for variable bead volumes' "
+        "Europhysics Letters DOI:10.1209/0295-5075/102/40009")
