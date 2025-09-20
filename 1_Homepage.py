298c3e627ae82fb2e8e81c878ce6b7e652993c51
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-09-20 18:58:59 +0200

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -6,6 +6,8 @@ st.title('Homepage')
 
 st.text('It provides a few tools for the derivation of DPD models')
 
+st.text('The Gaussian Apps describes a solution for fitting eqn 2 in 'Multicentered Gaussian-Based Potentials for Coarse-Grained Polymer Simulations: Linking Atomistic and Mesoscopic Scales'"
+
 st.text("The standard DPD parametrization App derives the parameters of DPD models using the Groot De Warren methodology. "
         "The reference is 'Dissipative particle dynamics: "
         "Bridging the gap between atomistic and mesoscopic simulation' "
